3f98d5817f9a06dfb6260dbb876ef073d46ec55b
samuelcpr <samuelcpr464@gmail.com>
2023-06-16 07:53:06 -0300

    chore: adicionando autenticação

--- src/modules/form/form.jsx
+++ src/modules/form/form.jsx
@@ -6,7 +6,13 @@ import { getFirestore, collection, addDoc, doc, onSnapshot, deleteDoc } from "fi
 
 
 const firebaseConfig = {
-
+  apiKey: "AIzaSyAdgsvIjt5eU_O80r4yvXEbEEONVBEVGas",
+  authDomain: "crud-produtos-5972b.firebaseapp.com",
+  projectId: "crud-produtos-5972b",
+  storageBucket: "crud-produtos-5972b.appspot.com",
+  messagingSenderId: "650147894625",
+  appId: "1:650147894625:web:b68d6780141c62a2f86120",
+  measurementId: "G-946D1HQFBM"
 };
 
 const app = initializeApp(firebaseConfig);
