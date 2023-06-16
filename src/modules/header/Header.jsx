5ff755234cf777774414fa8571fa8253b4623ebc
samuelcpr <samuelcpr464@gmail.com>
2023-06-16 09:00:20 -0300

    feat: criando header do projeto

---
+++ src/modules/header/Header.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import logo from '../../logo.svg';
+import './style.css';
+
+function Header() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Projeto desenvolvido em react.js
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Ler Mais
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
