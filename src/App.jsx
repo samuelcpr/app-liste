19e57823c3fe82fa89a51cc97941ff7768ce8a33
samuelcpr <samuelcpr464@gmail.com>
2023-06-16 08:55:12 -0300

    feat: criando arquivo app.js

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+import Form from './modules/form/form';
+import Header from './modules/header/Header';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        
+         <Form/>
+      </div>
+     
+    );
+  }
+}
+
+export default App;
