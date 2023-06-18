be2830f3956215c9daed2946101a73c70b7d91b6
samuelcpr <samuelcpr464@gmail.com>
2023-06-18 16:57:53 -0300

    feat: criando compo de exibição de preços

---
+++ src/components/TotalPreco/TotalPreco.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+
+
+
+
+
+
+const ValorTotal = ({ products, showBottomModal, setShowBottomModal }) => {
+  const loadProductDataForPrice = () => {
+    setShowBottomModal(true);
+  };
+
+  const calcularTotal = () => {
+    let total = 0;
+    products.forEach((product) => {
+      total += parseFloat(product.price);
+    });
+    return total;
+  };
+
+  return (
+    <div>
+      {showBottomModal && (
+        <div className="modal">
+          <div className="modal-content">
+            <h2>Valor Total</h2>
+            <p>Total: R${calcularTotal()}</p>
+            <button onClick={() => setShowBottomModal(false)} id="fecharTotal">
+              Fechar
+            </button>
+          </div>
+        </div>
+      )}
+      <div className="valorTotal">
+        <button onClick={loadProductDataForPrice} id="botaoTotal">
+          Valor Total
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ValorTotal;
+
