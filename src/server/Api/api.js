63d0176f1f5c3f43258d66fb413caee4221a0129
samuelcpr <samuelcpr464@gmail.com>
2023-06-18 17:00:59 -0300

    feat: criando componente para api

---
+++ src/server/Api/api.js
@@ -0,0 +1,16 @@
+
+
+
+const FirebaseConfig = {
+  apiKey: "AIzaSyAdgsvIjt5eU_O80r4yvXEbEEONVBEVGas",
+  authDomain: "crud-produtos-5972b.firebaseapp.com",
+  projectId: "crud-produtos-5972b",
+  storageBucket: "crud-produtos-5972b.appspot.com",
+  messagingSenderId: "650147894625",
+  appId: "1:650147894625:web:b68d6780141c62a2f86120",
+  measurementId: "G-946D1HQFBM"
+};
+
+
+
+export default FirebaseConfig;
