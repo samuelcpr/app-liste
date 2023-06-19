303aaf06aa4fce2481ba269c4ed174bbd69ae976
samuelcpr <samuelcpr464@gmail.com>
2023-06-19 09:07:41 -0300

    add mod

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Form from './modules/form/form';
-import Header from './modules/header/Header';
+
 
 
 class App extends Component {
