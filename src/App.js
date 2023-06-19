23c52f21444a73b44666d54596453a64684d45c7
samuelcpr <samuelcpr464@gmail.com>
2023-06-19 09:24:35 -0300

    add

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+import Form from './modules/form/form';
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        
+         <Form/>
+      </div>
+     
+    );
+  }
+}
+
+export default App;
