a72a3938ffbc3e80d1d735104d3300dfd34b72b1
samuelcpr <samuelcpr464@gmail.com>
2023-06-22 01:42:42 -0300

    chore: adicionando mascara de numeros em edit prod

--- src/components/atualizandoProd/atualizeProd.jsx
+++ src/components/atualizandoProd/atualizeProd.jsx
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
 import { doc, updateDoc } from "firebase/firestore";
 import { firestore } from "../../modules/form/form";
-import "./atualize.css"
+import "./atualize.css";
 import CurrencyFormat from "react-currency-format";
 
 const LoadProductDataForEdit = ({ products, deleteProduct, editProduct }) => {
   const [editProductId, setEditProductId] = useState(null);
   const [editedProductName, setEditedProductName] = useState("");
-  const [editedProductPrice, setEditedProductPrice] = useState("");
+  const [editedProductPrice, setEditedProductPrice] = useState("00,00");
   const [editedProductType, setEditedProductType] = useState("");
 
   const handleEditProduct = (product) => {
@@ -20,7 +20,7 @@ const LoadProductDataForEdit = ({ products, deleteProduct, editProduct }) => {
   const cancelEditProduct = () => {
     setEditProductId(null);
     setEditedProductName("");
-    setEditedProductPrice("");
+    setEditedProductPrice("00,00");
     setEditedProductType("");
   };
 
@@ -29,7 +29,7 @@ const LoadProductDataForEdit = ({ products, deleteProduct, editProduct }) => {
       const editedProduct = {
         name: editedProductName,
         price: editedProductPrice,
-        type: editedProductType
+        type: editedProductType,
       };
 
       const updatedDoc = doc(firestore, "products", editProductId);
@@ -42,66 +42,93 @@ const LoadProductDataForEdit = ({ products, deleteProduct, editProduct }) => {
     }
   };
 
+  const formatPrice = (value) => {
+    const numericValue = value.replace(/[^0-9]/g, "");
+    const decimalPart = numericValue.slice(-2);
+    const integerPart = numericValue.slice(0, -2);
+    const reversedIntegerPart = integerPart.split("").reverse().join("");
+    const formattedValue =
+      reversedIntegerPart
+        .replace(/(\d{3})/g, "$1.")
+        .split("")
+        .reverse()
+        .join("") +
+      "," +
+      decimalPart;
+    return formattedValue || "0,00 R$";
+  };
+
+  const handlePriceInput = (value) => {
+    const formattedPrice = formatPrice(value || "0");
+    setEditedProductPrice(formattedPrice);
+  };
+
   return (
     <div className="result">
-       <div className="Descricao">
-              <h3 id="price1">Nome</h3>
-              <h3 id="price2">Preço</h3>
-              <h3 id="price3">Tipo</h3>
-            </div>
-      {products && products.map((product) => (
-        <li key={product.id} id="itemMain">
-          {/* <a id="item">{product.id}</a> */}
-          {editProductId === product.id ? (
-            <>
-              <input
-              id="itemx"
-                type="text"
-                value={editedProductName}
-                onChange={(e) => setEditedProductName(e.target.value)}
-              />
-              <input
-              id="itemx"
-                type="text"
-                value={editedProductPrice}
-                onChange={(e) => setEditedProductPrice(e.target.value)}
-              />
-              <input
-              id="itemx"
-                type="text"
-                value={editedProductType}
-                onChange={(e) => setEditedProductType(e.target.value)}
-              />
-              <div className="containerBut" >
-              <button onClick={saveEditedProduct} id="itemx1">Salvar</button>
-              <button onClick={cancelEditProduct} id="itemx2">Cancelar</button>
-              </div>
-            </>
-          ) : (
-            <>
-              <a id="item2">{product.name}</a>
-              <CurrencyFormat
-                id="item2"
-                value={product.price}
-                displayType="text"
-                thousandSeparator="."
-                decimalSeparator=","
-                prefix="R$"
-                renderText={(formattedValue) => <a id="item2">{formattedValue}</a>}
-              />
-              <a id="item3">{product.type}</a>
-              <div className="excluirMain">
-                <button onClick={() => deleteProduct(product.id)} id="excluir">
-                  Excluir
-                </button>
-                <button onClick={() => handleEditProduct(product)} id="editar">
-                  Editar
-                </button>
-              </div>
-            </>
-          )}
-        </li>
-      ))}
+      <div className="Descricao">
+        <h3 id="price1">Nome</h3>
+        <h3 id="price2">Preço</h3>
+        <h3 id="price3">Tipo</h3>
+      </div>
+      {products &&
+        products.map((product) => (
+          <li key={product.id} id="itemMain">
+            {editProductId === product.id ? (
+              <>
+                <input
+                  id="itemx"
+                  type="text"
+                  value={editedProductName}
+                  onChange={(e) => setEditedProductName(e.target.value)}
+                />
+                <input
+                  id="itemx"
+                  type="text"
+                  value={editedProductPrice}
+                  placeholder="00,00 R$"
+                  onChange={(e) => handlePriceInput(e.target.value)}
+                />
+
+                <input
+                  id="itemx"
+                  type="text"
+                  value={editedProductType}
+                  onChange={(e) => setEditedProductType(e.target.value)}
+                />
+                <div className="containerBut">
+                  <button onClick={saveEditedProduct} id="itemx1">
+                    Salvar
+                  </button>
+                  <button onClick={cancelEditProduct} id="itemx2">
+                    Cancelar
+                  </button>
+                </div>
+              </>
+            ) : (
+              <>
+                <a id="item2">{product.name}</a>
+                <CurrencyFormat
+                  id="item2"
+                  value={product.price}
+                  displayType="text"
+                  thousandSeparator="."
+                  decimalSeparator=","
+                  prefix="R$"
+                  renderText={(formattedValue) => <a id="item2">{formattedValue}</a>}
+                />
+                <a id="item3">{product.type}</a>
+                <div className="excluirMain">
+                  <button onClick={() => deleteProduct(product.id)} id="excluir">
+                    Excluir
+                  </button>
+                  <button onClick={() => handleEditProduct(product)} id="editar">
+                    Editar
+                  </button>
+                </div>
+              </>
+            )}
+          </li>
+        ))}
     </div>
   );
 };
