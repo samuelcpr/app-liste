9246fec9622c59f40cf772db860034492c6d2492
samuelcpr <samuelcpr464@gmail.com>
2023-06-22 01:42:12 -0300

    delete: excluindo parte inutilizavel do código

--- src/components/AddProd/AddProd.jsx
+++ src/components/AddProd/AddProd.jsx
@@ -43,48 +43,8 @@ const ProductForm = ({ addProduct }) => {
     setPrice(formattedPrice);
   };
 
-  // Função de máscara de preço
-  function moeda(a, e, r, t) {
-    let n = "";
-    let h = 0;
-    let u = 0;
-    let l = "";
-    let o = window.Event ? t.which : t.keyCode;
-    if (13 === o || 8 === o) {
-      return true;
-    }
-    if (n = String.fromCharCode(o), -1 === "0123456789".indexOf(n)) {
-      return false;
-    }
-    for (u = a.value.length, h = 0; h < u && ("0" === a.value.charAt(h) || a.value.charAt(h) === r); h++);
-    for (l = ""; h < u; h++) {
-      if (-1 !== "0123456789".indexOf(a.value.charAt(h))) {
-        l += a.value.charAt(h);
-      }
-    }
-    if (l += n, 0 === (u = l.length) && (a.value = ""), 1 === u && (a.value = "0" + r + "0" + l), 2 === u && (a.value = "0" + r + l), u > 2) {
-      let ajd2 = "";
-      let j = 0;
-      for (let h = u - 3; h >= 0; h--) {
-        if (3 === j) {
-          ajd2 += e;
-          j = 0;
-        }
-        ajd2 += l.charAt(h);
-        j++;
-      }
-      a.value = "";
-      const tamanho2 = ajd2.length;
-      for (let h = tamanho2 - 1; h >= 0; h--) {
-        a.value += ajd2.charAt(h);
-      }
-      a.value += r + l.substr(u - 2, u);
-    }
-    return false;
-  }
-
-  
  
+  
   
 
   return (
